c06aac5c889fac439d7f859dab1ef5b1234d9c6b
JanRhoKa <92030143+JanRhoKa@users.noreply.github.com>
2021-11-12 11:40:47 +0100

    merge_weight_1 function added
    Added the merge_weight_1 function in the graph analysis. I think it works but we should add more testing. As the mapping in the iGraph contract_vertices function is a bit tricky and I generate nodes without values while merging as all possible nodes, which have to be deleted later on. 
    Added the docstrings and also generalized the plotting so there is a function, which can be called to plot the graphs at any point.

--- Meta_Graph_2.py
+++ Meta_Graph_2.py
@@ -4,12 +4,8 @@ import time
 import igraph as ig
 import numpy as np
 import pandas as pd
-import scanpy as sc
 import itertools
-import bokeh as bok
 import seaborn as sns
-import plotly
-import plotly.graph_objects as go
 
 start = time.time()
 
@@ -28,6 +24,7 @@ def relabel_clusterings(clusterings: pd.DataFrame) -> pd.DataFrame:
 
 
 # jaccrad index computation by Isaac
+@njit
 def clustering_edges_array(clustering1: "np.ndarray[int]",
                            clustering2: "np.ndarray[int]") -> "list[tuple[int, int, float]]":
     """
@@ -77,17 +74,35 @@ def clustering_edges_array(clustering1: "np.ndarray[int]",
     return edges
 
 
-def read_data(path):
-    # read data
+# read data
+def read_data(path, sample_size):
+    """
+    Reads the data from file and converts it to a pandas df, returns a sample or the complete data.
+
+    :param path: Path of the data.
+    :param sample_size: If not all data selected returns only a smaller sample for testing, type int or string ,
+        number of samples (int) or "all".
+    :return: Data as a pandas dataframe
+    """
     data = pd.read_table(path)
 
     # for testing return only a sample of clusters
-    return data.iloc[:, 0:21]
+    if type(sample_size) == int:
+        return data.iloc[:, 0:sample_size]
 
-    # return data
+    # return complete set
+    if sample_size == "all":
+        return data
 
 
-def build_graph(data, color_vertex_methode):
+def build_graph(data):
+    """
+    Builds a meta-graph based on the given data (list of clusterings).
+
+    Assigns each cluster for each clustering a node in the graph and calculates the edges (weighted) using the jaccard index.
+    :param data: Pandas dataframe from the single cell data.
+    :return: Returns an igraph object Graph.
+    """
     # create graph
     G = ig.Graph()
 
@@ -132,18 +147,130 @@ def build_graph(data, color_vertex_methode):
                 vertex_labels.append(edge_end)
                 vertex_cluster_methode.append(cluster_methode_1)
 
-
             # check if edge weight is zero
             if edge_weight != 0:
                 # add edge to graph
                 G.add_edges([(edge_start, edge_end)])
                 edge_labels.append(np.round(edge_weight, 5))
 
+    # add edge weight and name to graph
+    G.es["weight"] = edge_labels
+    G.vs["name"] = vertex_labels
+    G.vs["clustering"] = vertex_cluster_methode
+
+    return G
+
+
+# returns a list of all clusterings closest two the given number of clusters or with the number of cluster (if >= 2)
+def sort_by_number_clusters(settings, data, number_of_clusters):
+    """
+    Binning of the clusterings by number of clusters.
+
+    :param settings: Setting data from the sc clustering as a pandas df.
+    :param data: Clutering data from the sc data as a pandas df.
+    :param number_of_clusters: The number of clusters which should be binned together.
+    :return: Returns a list of the clusterings binned by the number of clusters, e.g. [C001, C002, ...].
+    """
+
+    # transforms settings to a dataframe with occurrence of the number of clusters
+    frequency = settings.groupby('n_clusters').count()
+    reindex_frequency = frequency.reset_index()
+
+    # check if number_of_clusters exists in the list of clustering settings
+    if not (number_of_clusters in np.unique(settings['n_clusters'].values)):
+        # if not take the two clusters two clusters closest to the number
+        closest_clusterings = reindex_frequency.iloc[
+            (reindex_frequency['n_clusters'] - number_of_clusters).abs().argsort()[:2]]
+
+        # number of cluster for the closest clusterings
+        number_of_clusters_closest = closest_clusterings.loc[:, "n_clusters"].values
+        # find all clusterings with that number of clusters
+        list_of_clusterings = settings.loc[settings['n_clusters'].isin(number_of_clusters_closest)]["id"].values
+
+        print("There are no clustering methods with ", number_of_clusters,
+              " clusters; the two closest clusterings are :", list_of_clusterings)
+
+    # number of clusters exist in settings
+    else:
+        # check if number of clusters occurs only once
+        if frequency.loc[number_of_clusters, "id"] == 1:
+            # if so take the two closest clusters of the selected number of clusters
+            closest_clusterings = reindex_frequency.iloc[
+                (reindex_frequency['n_clusters'] - number_of_clusters).abs().argsort()[:2]]
+
+            # number of cluster for the closest clusterings
+            number_of_clusters_closest = closest_clusterings.loc[:, "n_clusters"].values
+            # find all clusterings with that number of clusters
+            list_of_clusterings = settings.loc[settings['n_clusters'].isin(number_of_clusters_closest)]["id"].values
+
+            print("Clustering methods with ", number_of_clusters,
+                  " clusters  occurs only once; the two closest clusterings are :", list_of_clusterings)
+
+        # otherwise there are two or more clusterings with the same number of clusters
+
+        # number of clusters exists more than once
+        else:
+            # select all clusterings with the given number of clusters
+            list_of_clusterings = settings.loc[settings['n_clusters'] == number_of_clusters]["id"].values
+
+            print("Clustering methods with ", number_of_clusters, " clusters:", list_of_clusterings)
+
+    # return the list with all clusterings
+    return data[list_of_clusterings]
+
+
+
+def graph_analysis(G, analysis_methode):
+    """
+    A function for analysing the graph using different methods. At the moment more of a search for useful analysis.
+    E.g. merges all edges with edge weight 1, if analysis_methode = "merge_weight_1".
+
+    :param G: The graph which to analyse, iGraph object graph.
+    :param analysis_methode: The name of the function for the analysis, type string, e.g. "merge_weight_1".
+    :return: Returns the Graph after changing the edges or nodes depending on the chosen function.
+    """
+    if analysis_methode == "clique_number":
+        print(G.clique_number())
+
+    if analysis_methode == "girth":
+        print(G.girth())
+
+    if analysis_methode == "decompose":
+        decompose_G = G.decompose()
+        ig.plot(decompose_G[0])
+
+    if analysis_methode == "maximal_cliques":
+        print(G.cliques())
+
+    if analysis_methode == "merge_weight_1":
+        for edge in G.es:
+            if edge["weight"] == 1:
+                vertex_indices = range(G.vcount())
+                merge_list = [edge.source if x == edge.target else x for x in vertex_indices]
+                G.contract_vertices(merge_list, combine_attrs="first")
+
+        vertices_to_delete = [vertex.index for vertex in G.vs if vertex['name'] is None]
+        G.delete_vertices(vertices_to_delete)
+        G.simplify(combine_edges=max)
+
+        return G
+
+
+def plot_graph(G, label_on_off, color_vertex):
+    """
+    Function for plotting the Graph for all other used functions.
+
+    :param G: Graph that should be plotted.
+    :param label_on_off: Turns label on or off, get "label_off" or "label_on", type: string.
+    :param color_vertex: Gets the coloring method for the vertices, "degree" or "clustering", type: string.
+    :return: No return; only plots the graph.
+    """
+
     # color by degree
-    if color_vertex_methode == "degree":
+    if color_vertex == "degree":
         # calculate the degree of each vertex
         degree_graph = []
-        for vertex in vertex_labels:
+        for vertex in G.vs["name"]:
             degree_graph.append(G.degree(vertex))
 
         # color according to the degree
@@ -152,28 +279,44 @@ def build_graph(data, color_vertex_methode):
         G.vs['color'] = colors
 
     # color by clustering
-    if color_vertex_methode == "clustering":
-
+    if color_vertex == "clustering":
         # assign number to each clustering methode, e.g. "C001" = 0
         names_to_numbers = {}
         index_names = 0
-        for name in set(vertex_cluster_methode):
+        for name in set(G.vs["clustering"]):
             names_to_numbers[name] = index_names
             index_names = index_names + 1
-
-        color_palette = ig.ClusterColoringPalette(len(clusters.columns))
+        color_palette = ig.ClusterColoringPalette(len(names_to_numbers))
         # color the nodes by clustering methode using the numbers, e.g. "C001" = 0
-        colors = [color_palette[names_to_numbers[name]] for name in vertex_cluster_methode]
+        colors = [color_palette[names_to_numbers[name[0:4]]] for name in G.vs["name"]]
         G.vs['color'] = colors
 
-    layout = G.layout("kk")
-    ig.plot(G, layout=layout, vertex_size=20, vertex_label=vertex_labels, edge_label=edge_labels)
+    # layout = G.layout_fruchterman_reingold()
+    # layout = G.layout_kamada_kawai()
+    layout = G.layout_auto()
+
+    # turn labels on or off
+    if label_on_off == "label_on":
+        ig.plot(G, layout=layout, vertex_size=20, vertex_label=G.vs["name"], edge_label=G.es["weight"])
+    if label_on_off == "label_off":
+        ig.plot(G, layout=layout, vertex_size=20)
 
 
 # run program
-clustering_data = read_data("s2d1_clustering.tsv")
-build_graph(clustering_data, "clustering")
+# read data
+clustering_data = read_data("s2d1_clustering.tsv", "all")
+settings_data = read_data("s2d1_settings.tsv", "all")
+
+# build graph, G is used as the variable for the Graph internally
+number_of_clusters_data = sort_by_number_clusters(settings_data, clustering_data, 3)
+graph = build_graph(number_of_clusters_data)
+
+# analyse graph
+G = graph_analysis(graph, "merge_weight_1")
+plot_graph(G, "label_on", "degree")
+# merge_edges(graph)
 
 # measure the time
 end = time.time()
 print("Time to run: ", end - start)
+
