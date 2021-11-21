27d6cc48da102b6a3527c217204213b95f24e04f
JanRhoKa <92030143+JanRhoKa@users.noreply.github.com>
2021-11-21 13:58:59 +0100

    installable packages, test-function and partitioning
    Organized the diffrent parts of the project. Folder multires_consensus_clustering for scripts and function of the project, installable with flit.
    folder for test functions with toy data set, still in progress.
    added file for graph analysis with igraph partitioning functions.
    Main directory for running the functions of multires.

---
+++ multires_consensus_clustering/__init__.py
@@ -0,0 +1,8 @@
+"""
+Code for the multi-resolution consensus clustering project.
+"""
+__version__ = "0.0.1"
+
+from .binning import bin_n_clusters
+from .Meta_Graph import build_graph, plot_graph
+from .graph_analysis import merg_edges_weight_1, min_cuts, graph_community_detection
